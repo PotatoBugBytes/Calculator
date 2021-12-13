c07c0bf8950b0a8a548e8360fb6c93bce72e6b39
ThunderProgrammer777 <shloksoni2007@gmail.com>
2021-12-13 19:54:32 +0530

    bugs fixes - answer is not obtained by clicking "=" button. - Invalid Input function added so user can know that there input is incorrect (still in beta needs slot of improvement :)

---
+++ virus.py
@@ -0,0 +1,28 @@
+import math
+"""import tkinter as tk
+from tkinter import ttk
+from func import song1
+
+root = tk.Tk()
+root.title("VIRUS.exe")
+
+def true():
+    song1()
+
+name = tk.StringVar()
+
+cmd = ttk.Label(root,text="Name: ")
+cmd.pack(side='left',padx=(0,10))
+name_box = ttk.Entry(root,width=15,textvariable=name)
+name_box.pack(side='left')
+name_box.focus()
+quit_button = ttk.Button(root,text="Quit")
+quit_button.pack(side='right',fill="x"and"y",expand=True)
+printer = ttk.Scrollbar(root)
+
+root.mainloop()
+"""
+
+
+print(math.ceil(25215.23))
+print(math.floor(2.23))
